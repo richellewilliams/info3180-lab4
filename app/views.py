b4197239fd7ea7e97dae47e9bd2a29e32396adaa
Richelle Williams <richellewilliams3@gmail.com>
2023-02-24 10:02:03 -0500

    Imported send_from_directory to allow the actual images to show

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
